865baba826e842b6c9eecf27ce120c220bd497fb
egorov_av <egorov_av@mail.ru>
2023-06-16 17:36:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {NewsItem} from "./NewsItem/NewsItem";
+import {useEffect, useState} from "react";
 
+const initNews = [
+  { title: 'Первая новость', url: 'www.example.com', username: 'Пользователь 1', date: '10.10.11', score: 10, id: 1 },
+  { title: 'Вторая новость', url: 'www.example.com', username: 'Пользователь 2', date: '11.10.11', score: 100, id: 2 },
+  { title: 'Третья новость', url: 'www.example.com', username: 'Пользователь 3', date: '12.10.11', score: 20, id: 3 }
+]
+
+const newNews = {
+  title: 'Новая новость',
+  url: 'www.example.com',
+  username: 'Пользователь',
+  date: '10.10.11', score: 1,
+  id: 4
+}
 function App() {
+
+  const [news, setNews] =
+      useState(JSON.parse(window.localStorage.getItem("keyNews")) || initNews);
+
+  useEffect(() => {
+    window.localStorage.setItem("keyNews", JSON.stringify(news));
+  }, [news]);
+
+  const newsCountHandler = () => {
+    setNews((prevState) => [...prevState, newNews]);
+    console.log(news.length);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div>Колличество новостей: {news.length}</div>
+        <button onClick={newsCountHandler}>Добавить новость.</button>
+        {
+          news.map(item => {
+            return (<NewsItem
+                    key={item.id}
+                    title={item.title}
+                    url={item.url}
+                    username={item.username}
+                    date={item.date} score={item.score}
+                />
+            )
+          })
+        }
+      </>
   );
 }
 
